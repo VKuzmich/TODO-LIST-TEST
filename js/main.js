12bba8ff1f6a622ef6941fd8c6278efcfcf2d13c
«Vladimir <vjk1976@ukr.net>
2019-02-25 22:32:48 +0200

    EDITING AND ADDING BUTTONS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 
 
 
+// $(function() {
+
+//   $('input[name="datefilter"]').daterangepicker({
+//       autoUpdateInput: false,
+//       locale: {
+//           cancelLabel: 'Clear'
+//       }
+//   });
+
+//   $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+//       $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+//   });
+
+//   $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+//       $(this).val('');
+//   });
+
+// });
+
 
 // List with handle
 Sortable.create(listWithHandle, {
@@ -18,7 +37,7 @@ addListenerToAddTask();
                 return false
             }
             var containerTodo = 
-            "<div class='row row-task'><div class='col-xsm col1-task d-flex align-items-center justify-content-center'><div class='form-check d-flex align-items-center'><input class='form-check-input' type='checkbox' value='' id='defaultCheck1'><label class='form-check-label' for='defaultCheck1'></label></div></div><div class='col col2-task'><p class='task font-weight-light'>" + valueTask + "</p></div><div  class='col-xsm col3-mixer d-flex align-items-center justify-content-center'><div id='row3-mixer' class='row'><div class='col col3-sort'><button class='btn btn-sort'><i class='fas fa-sort'></i></div> <div class='col col3-separator1'><span class='separator separator-task1'></span></div><div class='col col3-pencil'> <button class='btn btn-pencil' ><i class='fas fa-pencil-alt'></i></button></div><div class='col col3-separator2'><span class='separator separator2'></span></div><div class='col col3-trashcan'><button id='trash' class='btn btn-trash' ><i class='far fa-trash-alt'></i></div></div></div></div>";
+            "<div id='todo-table' class='row row-task'><div class='col-xsm col1-task d-flex align-items-center justify-content-center'><div class='form-check d-flex align-items-center'><input class='form-check-input' type='checkbox' value='' id='defaultCheck1'><label class='form-check-label' for='defaultCheck1'></label></div></div><div class='col col2-task'><p class='task font-weight-light'>" + valueTask + "</p></div><div  class='col-xsm col3-mixer d-flex align-items-center justify-content-center'><div id='row3-mixer' class='row'><div class='col col3-sort'><button class='btn btn-sort'><i class='fas fa-sort'></i></div> <div class='col col3-separator1'><span class='separator separator-task1'></span></div><div class='col col3-pencil'> <button class='btn btn-pencil' ><i class='fas fa-pencil-alt'></i></button></div><div class='col col3-separator2'><span class='separator separator2'></span></div><div class='col col3-trashcan'><button id='trash' class='btn btn-trash' ><i class='far fa-trash-alt'></i></div></div></div></div>";
             $(".list-group").append(containerTodo);
             $(".input-task").val("");
             addListenerToDeleteTask();
@@ -44,7 +63,7 @@ function addListenerToDeleteTask(){
     addListenerToEditTask();
 function addListenerToEditTask(){
     $(".btn-pencil").click(function(){
-    var valueEditTask = $(this).parents('.task-head').find("p");
+    var valueEditTask = $(this).parents("#todo-table").find("p");
     $("#myModal").modal("show");
     var inputForEdit = $(".input-task-edit");
     inputForEdit.val(valueEditTask.text());
@@ -60,21 +79,21 @@ function addListenerToEditTask(){
     });
     }
 
-    addListenerToEditTask1();
-function addListenerToEditTask1(){
-    $(".btn-pencil").click(function(){
-    var valueEditTask = $(this).parents('.list-group').find("p");
-    $("#myModal").modal("show");
-    var inputForEdit = $(".input-task-edit");
-    inputForEdit.val(valueEditTask.text());
-    $(".save-changes").unbind("click");
-    $(".save-changes").click(function(){
-        if(!inputForEdit.val().trim()){
-            alert("Please, enter your text!")
-            return false
-        }
-        valueEditTask.text(inputForEdit.val());
-    });
+//     addListenerToEditTask();
+// function addListenerToEditTask(){
+//     $(".btn-pencil").click(function(){
+//     var valueEditTask = $(this).parents('.task-head').find("p");
+//     $("#myModal").modal("show");
+//     var inputForEdit = $(".input-task-edit");
+//     inputForEdit.val(valueEditTask.text());
+//     $(".save-changes").unbind("click");
+//     $(".save-changes").click(function(){
+//         if(!inputForEdit.val().trim()){
+//             alert("Please, enter your text!")
+//             return false
+//         }
+//         valueEditTask.text(inputForEdit.val());
+//     });
     
-    });
-    }
+//     });
+//     }
